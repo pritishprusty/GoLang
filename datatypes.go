90d347896d790ccef2059c1988381c69403ab57b
Pritish <54237257+pritishprusty@users.noreply.github.com>
2022-01-19 09:09:58 +0530

    datatypes.go

---
+++ datatypes.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//main func will be automatically called when you start the application
+func main() {
+	var data int
+	data = 10
+	// by giving : its not req to define the data type as we did above
+	data1 := 1000
+	data2 := "hello"
+	// data:= 1000 is redeclaring data variables so will throw an error
+	//data :=1000
+	//if a var is not used then go will throw an error
+	fmt.Print(data)
+	fmt.Println(data1)
+	fmt.Println(data2)
+	// data types
+	//1.primitive
+	//int, float64, string, bool, complex
+	//2. non-primitive
+	//struct, map, chan, interface,array, slice(like dynamic list array), rune, reflect, pointer
+	// functional programming
+	//
+}
