cca33368912008972e1f02f37cc14469a82c0761
Pritish <54237257+pritishprusty@users.noreply.github.com>
2022-01-20 10:18:08 +0530

    func2.go

---
+++ Practice1.go
@@ -0,0 +1,28 @@
+/*
+alpha:= []string{'a','b','c'} normal way to initialize array 
+alpha:= make([]string,0,26) make makes the array dynamic and then the data type
+then an optional parameter and then the length of the araay
+
+digit:=[]int{1,2,3,4}
+
+
+
+*/
+
+
+
+package main
+func main {
+	
+   
+
+ var name[]string
+ alphabet := make([]string , 0,26)
+ digit:=make([]int,0,10)
+ var i byte
+for {i='a';i<'z';i++
+	digit={i}
+	fmt.Println(i)}
+}
+
+
---
+++ func2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func greetings(name string) (string, error) { //the last string is the return type
+	if name == "" {
+		return name, errors.New("Name can not be Null")
+	}
+	return "Hello," + name, nil
+}
+
+func add(num ...int) int { // the three dots here represents multiple values that can go into num and its dynamic (like list or array but it does not has index)
+	sum := 0
+	for value := range num {
+		sum += value
+	}
+	return sum
+
+}
+
+func main() {
+	greetings, err := greetings("Pritish")
+	if err != nil {
+		fmt.Println(err)
+
+	} else {
+		fmt.Println(greetings)
+	}
+
+	fmt.Println("the sum of 12345 is ", add(1, 2, 3, 4, 5))
+	fmt.Println("the sum of 1,2,3,4,5,", add(1, 2, 3, 4, 5, 6))
+
+}
